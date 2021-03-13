d36d3568a6b3a29eb3a893aed7369679bf2a30a4
prachius <70451555+prachius@users.noreply.github.com>
2021-03-14 04:52:58 +0530

    Update services.js

--- src/components/Front/Services/services.js
+++ src/components/Front/Services/services.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Icon1 from "./Images/code.svg";
-import Icon2 from "./Images/read.svg";
+import Icon2 from "./Images/fashion.svg";
 import Icon3 from "./Images/cook.svg";
 import { Container, H1, Wrapper, ServicesCard, ServicesIcon, H2, P } from "./servicesStyles";
 
@@ -13,17 +13,17 @@ const Services = () => {
                     <ServicesCard>
                         <ServicesIcon src={Icon1} />
                         <H2>Technology</H2>
-                        <P>Lorem ipsum dolor sit amet consectetur adipisicing.</P>
+                        <P>Learning HTML and CSS is a lot more challenging than it used to be. Thereby we present you our platform to pave your way to professionalism. </P>
                     </ServicesCard>
                     <ServicesCard>
                         <ServicesIcon src={Icon2} />
-                        <H2>Build</H2>
-                        <P>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et?</P>
+                        <H2>Design </H2>
+                        <P>Fashion is a popular aesthetic expression. Discover your style of dressing here.</P>
                     </ServicesCard>
                     <ServicesCard>
                         <ServicesIcon src={Icon3} />
-                        <H2>Skill</H2>
-                        <P>Lorem ipsum dolor sit amet consectetur adipisicing elit.</P>
+                        <H2>Taste</H2>
+                        <P>No one who cooks, cooks alone. Figure out our new  and try out new tastes.</P>
                     </ServicesCard>
                 </Wrapper>
             </Container>
