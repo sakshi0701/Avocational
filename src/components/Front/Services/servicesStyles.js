88cde8f2c941d93baad32ee3a95d538d0a98659b
prachius <70451555+prachius@users.noreply.github.com>
2021-03-14 04:54:42 +0530

    Update servicesStyles.js

--- src/components/Front/Services/servicesStyles.js
+++ src/components/Front/Services/servicesStyles.js
@@ -1,80 +1,34 @@
-import styled from "styled-components";
+import React from 'react';
+import Icon1 from "./Images/code.svg";
+import Icon2 from "./Images/fashion.svg";
+import Icon3 from "./Images/cook.svg";
+import { Container, H1, Wrapper, ServicesCard, ServicesIcon, H2, P } from "./servicesStyles";
 
-export const Container = styled.div`
-height: 800px;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-background: #00B0FF;
-
-@media screen and (max-width: 768px) {
-    height: 1100px;
-
-@media screen and (max-width: 480px) {
-    height: 1300px;
+const Services = () => {
+    return (
+        <>
+            <Container id="services">
+                <H1>What to find?</H1>
+                <Wrapper>
+                    <ServicesCard>
+                        <ServicesIcon src={Icon1} />
+                        <H2>Technology</H2>
+                        <P>Learning HTML and CSS is a lot more challenging than it used to be. Thereby we present you our platform to pave your way to professionalism. </P>
+                    </ServicesCard>
+                    <ServicesCard>
+                        <ServicesIcon src={Icon2} />
+                        <H2>Design </H2>
+                        <P>Fashion is a popular aesthetic expression. Discover your style of dressing here.</P>
+                    </ServicesCard>
+                    <ServicesCard>
+                        <ServicesIcon src={Icon3} />
+                        <H2>Taste</H2>
+                        <P>No one who cooks, cooks alone. Figure out our new  and try out new tastes.</P>
+                    </ServicesCard>
+                </Wrapper>
+            </Container>
+        </>
+    )
 }
-`;
 
-export const H1 = styled.h1`
-font-size: 3rem;
-color: white;
-margin-bottom: 64px;
-font-family: 'Lobster', cursive;
-
-@media screen and (max-width: 480px) {
-    font-size: 2rem;
-}
-`;
-
-export const Wrapper = styled.div`
-max-width: 1000px;
-margin : 0 auto;
-display: grid;
-grid-template-columns: 1fr 1fr 1fr;
-align-items: center;
-grid-gap: 16px;
-padding 0 50px;
-
-@media screen and (max-width: 1000px) {
-    grid-template-columns: 1fr 1fr;
-
-@media screen and (max-width: 768px) {
-    grid-template-columns: 1fr;
-    padding: 0 20px;
-`;
-
-export const ServicesCard = styled.div`
-background: white;
-display: flex;
-flex-direction: column;
-justify-content: flex-start;
-align-items: center;
-border-radius: 10px;
-max-height: 340px;
-padding: 30px;
-box-shadow: 0 1px 3px rgba(0,0,0,0.2);
-transition: all 0.2s ease-in-out;
-
-&:hover {
-    transform: scale(1.02);
-    transition: all 0.2s ease-in-out;
-    cursor: pointer;
-}
-`;
-
-export const ServicesIcon = styled.img`
-height: 160px;
-width: 160px;
-margin-bottom: 10px;
-`;
-
-export const H2 = styled.h2`
-font-size: 1rem;
-margin-bottom: 10px;
-`;
-
-export const P = styled.p`
-font-size: 1rem;
-text-align: center;
-`;
+export default Services
