a9bea6d6e9e215e777a856e46d0c15c85850b71d
prachius <70451555+prachius@users.noreply.github.com>
2021-03-14 04:55:41 +0530

    Update Signup.js

--- src/components/Front/Signup.js
+++ src/components/Front/Signup.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
+import styled from "styled-components";
 
 export default function Signup() {
   const emailRef = useRef()
@@ -33,28 +34,30 @@ export default function Signup() {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-5">Sign Up</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
+      <Card className="d-flex justify-content-center align-items-center">
+        <SignupBox>
+          <Card.Body>
+            <h2 className="text-center mb-5">Sign Up</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control type="password" ref={passwordConfirmRef} required />
+              </Form.Group>
+              <Button disabled={loading} className="w-100 btn-info" type="submit">
+                Sign Up
             </Button>
-          </Form>
-        </Card.Body>
+            </Form>
+          </Card.Body>
+        </SignupBox>
       </Card>
       <div className="w-100 text-center mt-2 mb-4">
         Already have an account? <Link to="/login">Log In</Link>
@@ -62,3 +65,9 @@ export default function Signup() {
     </>
   )
 }
+const SignupBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 800px;
+`;
